897fef69af088d8841d36c46ef17642ee970bff0
Sören Lemke <61249756+soerenlemke@users.noreply.github.com>
2026-01-31 21:37:53 +0100

    adding support for newlines to terminal driver

--- src/kernel.c
+++ src/kernel.c
@@ -86,6 +86,17 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 void terminal_putchar(char c) 
 {
+	if (c == '\n') {
+		terminal_column = 0;
+		if (++terminal_row == VGA_HEIGHT)
+			terminal_row = 0;
+		return;
+	}
+	if (c == '\r') { // carriage return// carriage return
+		terminal_column = 0;
+		return;
+	}
+
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -111,5 +122,5 @@ void kernel_main(void)
 	terminal_initialize();
 
 	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Hello,\nkernel\nWorld!\n");
 }
